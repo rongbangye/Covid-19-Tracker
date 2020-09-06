308add953315452c083be2ccbe7bc04033c497c4
Solomon <solomonmeresa8@gmail.com>
2020-09-05 22:06:18 -0700

    adds modal component

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,37 @@ var totalDeathEl = document.querySelector("#total-death");
 var countryInputName = document.querySelector("#country-input");
 var searchBtn = document.querySelector("#search-btn");
 
+// ----  Modal DOM Elements---
+const modal = document.querySelector("#modal");
+const closeBtn = document.querySelector(".close");
+const modalBody = document.querySelector(".modal-body");
+
+// Modal Events
+closeBtn.addEventListener("click", closeModal);
+window.addEventListener("click", outsideModalClick);
+
+// Open Modal
+function openModal(str) {
+  modal.style.display = "block";
+  var messageEl = document.createElement("p");
+  messageEl.textContent = str;
+  modalBody.appendChild(messageEl);
+}
+
+// Close Modal
+function closeModal() {
+  modal.style.display = "none";
+  modalBody.textContent = "";
+}
+
+// Close If Outside Click
+function outsideModalClick(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+    modalBody.textContent = "";
+  }
+}
+
 var getCountryData = function (country) {
   var urlCountry =
     "https://disease.sh/v3/covid-19/countries/" +
@@ -17,6 +48,11 @@ var getCountryData = function (country) {
   // fetch data for each country
   fetch(urlCountry).then((response) => {
     response.json().then((data) => {
+      // if there is no response open modal with the error message
+      if (data.message) {
+        openModal(data.message);
+      }
+      // if there is response display data
       displayDataToTheUI(data);
     });
   });
@@ -40,7 +76,7 @@ var formSubmitHandler = function (event) {
 
     countryInputName.value = "";
   } else {
-    alert("Please enter a correct country name");
+    openModal("Input field can not be empty");
   }
 };
 
