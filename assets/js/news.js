f3465ad6f2d06cc0cb6cc51e0b3d7aea5ff0b1ac
rongbang <rongbang.ye@gmail.com>
2020-09-11 23:56:52 -0700

    replaced news api

--- assets/js/news.js
+++ assets/js/news.js
@@ -3,21 +3,15 @@ const nextNews = document.querySelector(".next-news");
 
 // https://developer.smartable.ai/api-details#api=coronavirus&operation=stats
 function getNews() {
-  fetch("https://api.smartable.ai/coronavirus/news/US", {
-    method: "GET",
-    headers: {
-      "Cache-Control": "no-cache",
-      "Subscription-Key": "3009d4ccc29e4808af1ccc25c69b4d5d"
-    }
-  })
-    .then((response) => response.json())
-    .then((newsCovid) => {
+  fetch("https://cryptic-ravine-96718.herokuapp.com/")
+    .then(response => response.json())
+    .then(newsCovid => {
       const newsDetail = document.querySelector(".news-detail");
 
       let page = 0;
       let newsList = [];
-
-      newsCovid.news.forEach((news) => {
+      console.log(newsCovid);
+      newsCovid.news.forEach(news => {
         let liveCountryEL = document.createElement("li");
 
         liveCountryEL.classList = "news-detail-li";
@@ -25,7 +19,7 @@ function getNews() {
         liveCountryEL.innerHTML += `
         <p class="news-title">${truncateString(news.title, 80)}</p>
         <div class="readMoreNews"><a href="${
-          news.webUrl
+          news.link
         }" target="_blank">Read More <span>&#8594;</span> </a></div>
         `;
 
@@ -35,14 +29,16 @@ function getNews() {
 
       // pre and next button logic
       for (let i = 0; i < page + 1; i++) {
+        document.querySelector(".spinner-news").style.display = "none";
         newsDetail.appendChild(newsList[i]);
       }
 
       nextNews.addEventListener("click", () => {
+        console.log(page);
         // disable or enable next and previous button when reach on first or last page
         page == 0
           ? prevNews.classList.remove("disable")
-          : page == 28
+          : page == 99
           ? nextNews.classList.add("disable")
           : nextNews.classList.remove("disable");
 
